6d0c23dfe0e16694159d491387b594fb2e560f2e
gonzalo <gonzalo.sanchezcano@gmail.com>
2015-04-18 15:30:29 -0300

    grafo

--- Funcional/src/Grafo.hs
+++ Funcional/src/Grafo.hs
@@ -10,40 +10,62 @@ instance (Show a) => Show (Grafo a) where
 
 -- Ejercicio 1
 vacio :: Grafo a
-vacio = undefined
+vacio = G [] (\x -> [])
 
 -- Ejercicio 2
 nodos :: Grafo a -> [a]
-nodos = undefined
+nodos (G ns r) = ns 
 
 -- Ejercicio 3
 vecinos :: Grafo a -> a -> [a]
-vecinos = undefined
+vecinos (G ns r) n = r n
 
 -- Ejercicio 4
 agNodo :: a -> Grafo a -> Grafo a
-agNodo = undefined
+agNodo n (G ns r) = G (n:ns) r
 
 -- Ejercicio 5
-sacarNodo :: a -> Grafo a -> Grafo a
-sacarNodo = undefined
+sacarNodo :: Eq a => a -> Grafo a -> Grafo a
+sacarNodo n (G ns r) = 
+	G (nodosSin ns n) (relacionesSin r n)
+	where 
+		nodosSin ns n = filter (\m -> not (m == n)) ns 
+		relacionesSin r n = \x -> if (x == n) then [] else (r n)
 
+--PRE: n, m in nodos G
 -- Ejercicio 6
-agEje :: (a,a) -> Grafo a -> Grafo a
-agEje = undefined
+agEje :: Eq a => (a,a) -> Grafo a -> Grafo a
+agEje (n, m) (G ns r) = 
+	G ns (relacionesCon r (n, m))
+	where relacionesCon r (n, m) = \x -> if (x == n) 
+		then m:(r n) 
+		else (r n)
 
 -- Ejercicio 7
-lineal :: [a] -> Grafo a
-lineal = undefined
+lineal :: Eq a => [a] -> Grafo a
+lineal ns 		= 
+	G ns (relLin ns)
+	where
+		len = length ns
+		relLin ns = \x -> if ((len == 0) || (last ns == x))
+			then [] 
+			else [ns!!(y+1)| y <- [0.. len-1], ns!!y == x]
+		
 
 -- Ejercicio 8
-union :: Grafo a -> Grafo a -> Grafo a
-union = undefined
+union :: Eq a => Grafo a -> Grafo a -> Grafo a
+union (G ns1 r1) (G ns2 r2) = G (sinRepetidos (ns1++ns2)) (unionRel r1 r2)
+	where unionRel vecinos1 vecinos2 = \x -> sinRepetidos ((vecinos1 x) ++ (vecinos2 x))
 
 -- Ejercicio 9
 clausura :: Grafo a -> Grafo a
-clausura = undefined
+clausura (G ns r) = G ns (relClaus ns r)
+	where 
+		relClaus ns r = \x -> completarRel (relAlcanzadasPor x ns r)
+		relAlcanzadasPor x ns r = []
 
+sinRepetidos :: Eq a => [a] -> [a]
+sinRepetidos xs = [xs!!i | i <- [0..(length xs)-1], not (elem (xs!!i) (drop (i+1) xs))]
 
 
 
